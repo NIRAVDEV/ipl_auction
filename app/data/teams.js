08460d1cc6a31642a450649730256a0204d7c5ee
NIRAV DIPAKKUMAR PATEL <101976669+NIRAVDEV@users.noreply.github.com>
2025-12-21 10:36:54 +0530

    Add teams data to teams.js

---
+++ app/data/teams.js
@@ -0,0 +1,12 @@
+export const teams = [
+  { id: 'csk', name: 'CSK', color: 'bg-yellow-500', logo: '/logos/csk.png' },
+  { id: 'mi', name: 'MI', color: 'bg-blue-600', logo: '/logos/mi.png' },
+  { id: 'rcb', name: 'RCB', color: 'bg-red-600', logo: '/logos/rcb.png' },
+  { id: 'kkr', name: 'KKR', color: 'bg-purple-700', logo: '/logos/kkr.png' },
+  { id: 'gt', name: 'GT', color: 'bg-indigo-900', logo: '/logos/gt.png' },
+  { id: 'srh', name: 'SRH', color: 'bg-orange-500', logo: '/logos/srh.png' },
+  { id: 'lsg', name: 'LSG', color: 'bg-cyan-600', logo: '/logos/lsg.png' },
+  { id: 'dc', name: 'DC', color: 'bg-blue-400', logo: '/logos/dc.png' },
+  { id: 'pbks', name: 'PBKS', color: 'bg-red-500', logo: '/logos/pbks.png' },
+  { id: 'rr', name: 'RR', color: 'bg-pink-500', logo: '/logos/rr.png' },
+];
